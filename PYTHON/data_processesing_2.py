05a7c1e7a72a7efab636a38e7a31645ec3635655
Andrew Wentzel <tehwentzel@gmail.com>
2019-01-23 09:03:41 -0600

    added list of outliers

--- PYTHON/data_processesing_2.py
+++ PYTHON/data_processesing_2.py
@@ -9,6 +9,29 @@ from re import findall
 import numpy as np
 import pandas as pd
 
+ORGAN_LIST = [
+    'Brainstem','Cricoid_cartilage','Cricopharyngeal_Muscle',
+    'Esophagus','Extended_Oral_Cavity','Genioglossus_M',
+    'Glottic_Area','Hard_Palate','Hyoid_bone',
+    'IPC','Larynx','Lower_Lip',
+    'Lt_Ant_Digastric_M','Lt_Anterior_Seg_Eyeball',
+    'Lt_Brachial_Plexus','Lt_Lateral_Pterygoid_M',
+    'Lt_Masseter_M','Lt_Mastoid',
+    'Lt_Medial_Pterygoid_M','Lt_Parotid_Gland',
+    'Lt_Posterior_Seg_Eyeball','Lt_Sternocleidomastoid_M',
+    'Lt_Submandibular_Gland','Lt_thyroid_lobe',
+    'Mandible','MPC','Mylogeniohyoid_M',
+    'Rt_Ant_Digastric_M','Rt_Anterior_Seg_Eyeball',
+    'Rt_Brachial_Plexus','Rt_Lateral_Pterygoid_M',
+    'Rt_Masseter_M','Rt_Mastoid',
+    'Rt_Medial_Pterygoid_M','Rt_Parotid_Gland',
+    'Rt_Posterior_Seg_Eyeball','Rt_Sternocleidomastoid_M',
+    'Rt_Submandibular_Gland','Rt_thyroid_lobe',
+    'Soft_Palate','SPC','Spinal_Cord',
+    'Supraglottic_Larynx','Thyroid_cartilage','Tongue',
+    'Upper_Lip','GTVp','GTVn'
+]
+
 #sorts by size of largest integer string, which is the id for our files
 file_sort = lambda x: sorted(x, key =
                              lambda file: max([int(x) for x in findall("[0-9]+", file)])
